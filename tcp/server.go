d26a21f688a80df50a1f81b58be8429f02bd78e7
luxcgo <luxcgo@gmail.com>
2022-07-31 00:04:30 +0800

    tcp: server fulfilled

--- tcp/server.go
+++ tcp/server.go
@@ -3,6 +3,10 @@ package tcp
 import (
 	"context"
 	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
 	"github.com/luxpo/redisx/interface/tcp"
 	"github.com/luxpo/redisx/lib/logger"
@@ -13,25 +17,49 @@ type Config struct {
 }
 
 func ListenAndServeWithSignal(cfg *Config, handler tcp.Handler) error {
-	_, err := net.Listen("tcp", cfg.Address)
+	closeChan := make(chan struct{})
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		<-sigChan
+		closeChan <- struct{}{}
+	}()
+
+	listener, err := net.Listen("tcp", cfg.Address)
 	if err != nil {
 		return err
 	}
 	logger.Info("start listening...")
+	ListenAndServe(listener, handler, closeChan)
 
 	return nil
 }
 
 func ListenAndServe(listener net.Listener, handler tcp.Handler, closeChan <-chan struct{}) {
+	go func() {
+		<-closeChan
+		logger.Info("shutting down...")
+		listener.Close()
+		handler.Close()
+	}()
+
+	defer func() {
+		listener.Close()
+		handler.Close()
+	}()
 	ctx := context.Background()
+	var wg sync.WaitGroup
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			break
 		}
 		logger.Info("accept link")
+		wg.Add(1)
 		go func() {
+			defer wg.Done()
 			handler.Handle(ctx, conn)
 		}()
 	}
+	wg.Wait()
 }
