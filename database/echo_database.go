8649eedd86e0a28ecbeae2e5c877c58ca0351a2b
luxcgo <luxcgo@gmail.com>
2022-08-02 00:30:13 +0800

    database: echo db for test

---
+++ database/echo_database.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"github.com/luxpo/redisx/interface/resp"
+	"github.com/luxpo/redisx/lib/logger"
+	"github.com/luxpo/redisx/resp/reply"
+)
+
+type EchoDatabase struct {
+}
+
+func NewEchoDatabase() *EchoDatabase {
+	return &EchoDatabase{}
+}
+
+func (e EchoDatabase) Exec(client resp.Connection, args [][]byte) resp.Reply {
+	return reply.MakeMultiBulkReply(args)
+
+}
+
+func (e EchoDatabase) AfterClientClose(c resp.Connection) {
+	logger.Info("EchoDatabase AfterClientClose")
+}
+
+func (e EchoDatabase) Close() {
+	logger.Info("EchoDatabase Close")
+
+}
